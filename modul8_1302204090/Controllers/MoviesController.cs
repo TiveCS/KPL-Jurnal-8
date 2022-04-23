f96fb13238b53bfcb54036904fe2f4f85571cbed
TiveCS <creativecode.id@gmail.com>
2022-04-23 16:00:36 +0700

    empty template api

---
+++ modul8_1302204090/Controllers/MoviesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace modul8_1302204090.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class MoviesController : ControllerBase
+	{
+
+		[HttpGet]
+		public IEnumerable<Movie> GetMovies()
+		{
+			List<Movie> movies = new List<Movie>();
+
+			return movies;
+		}
+
+		[HttpPost]
+		public void AddMovie(Movie movie)
+		{
+
+		}
+
+		[HttpGet]
+		[Route("{id}")]
+		public Movie GetMovie(int id)
+		{
+			return null;
+		}
+		
+		[HttpDelete]
+		[Route("{id}")]
+		public void DeleteMovie(int id)
+		{
+
+		}
+	}
+}
---
+++ modul8_1302204090/Movie.cs
@@ -0,0 +1,19 @@
+﻿namespace modul8_1302204090
+{
+	public class Movie
+	{
+
+		public string Title { get; set; }
+
+		public string Director { get; set; }
+
+		public List<string> Stars { get; set; }
+
+		public string Description { get; set; }
+
+		public Movie()
+		{
+
+		}
+	}
+}
